5b1b833de7ffa26bb363b84cd4497a63368f579d
hehaosen <307052084@qq.com>
2016-10-13 10:02:51 +0800

    dd

--- public/javascripts/build/index.js
+++ public/javascripts/build/index.js
@@ -3,7 +3,7 @@
 // 选择器
 var $ = function $(_ele) {
 
-    if (!document.querySelector()) {
+    if (!document.querySelector) {
         if (_ele.substring(0, 1) === '#') {
             return document.getElementById(_ele.substring(1));
         } else if (_ele.substring(0, 1) === '.') {
@@ -17,15 +17,22 @@ var $ = function $(_ele) {
 // loding icon 动画效果
 var loadingAnimation = function loadingAnimation() {
     var ele = $('#J_loading');
-    var rotate = 90;
-    setTimeout(function () {
-        ele.cssText = 'transform: rotate(' + _rotate + 'deg)';
-        rotate += 90;
-        if (rotate === 360) {
-            rotate = 90;
+    var eleChild = ele.querySelector('.loading-icon');
+    var rotate = 90,
+        topOffset = 0;
+    setInterval(function () {
+        ele.style.cssText = 'transform: rotate(' + rotate + 'deg)';
+        eleChild.style.cssText = 'top:-' + topOffset + 'px';
+        topOffset += 32;
+
+        if (topOffset > 64) {
+            topOffset = 0;
+            rotate += 90;
+            if (rotate === 360) {
+                rotate = 0;
+            }
         }
-    }, 500);
-    console.log(ele);
+    }, 100);
 };
 
 // init
--- public/javascripts/es6/index.js
+++ public/javascripts/es6/index.js
@@ -1,7 +1,7 @@
 // 选择器
 var $ = (_ele) => {
 
-    if (!document.querySelector()) {
+    if (!document.querySelector) {
         if (_ele.substring(0, 1) === '#') {
             return document.getElementById(_ele.substring(1));
         } else if (_ele.substring(0, 1) === '.'){
@@ -15,15 +15,21 @@ var $ = (_ele) => {
 // loding icon 动画效果
 var loadingAnimation = () => {
     let ele = $('#J_loading');
-    let rotate = 90;
-    setTimeout(function(){
-        ele.cssText = `transform: rotate(${_rotate}deg)`;
-        rotate += 90;
-        if (rotate === 360) {
-            rotate = 90;
+    let eleChild = ele.querySelector('.loading-icon');
+    let rotate = 90, topOffset = 0;
+    setInterval(function(){
+        ele.style.cssText = `transform: rotate(${rotate}deg)`;
+        eleChild.style.cssText = `top:-${topOffset}px`;
+        topOffset += 32;
+
+        if (topOffset > 64) {
+            topOffset = 0;
+            rotate += 90;
+            if (rotate === 360) {
+                rotate = 0;
+            }
         }
-    },500);
-    console.log(ele);
+    }, 100);
 };
 
 // init
